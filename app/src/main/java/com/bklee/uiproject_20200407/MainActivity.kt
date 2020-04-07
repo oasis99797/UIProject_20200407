15d17c8edd6dbde2b0785c37bf9ba208fc18ce55
oasis99797 <oasis99797@gmail.com>
2020-04-07 21:54:06 +0900

    스트링 가공 예시

--- app/src/main/java/com/bklee/uiproject_20200407/MainActivity.kt
+++ app/src/main/java/com/bklee/uiproject_20200407/MainActivity.kt
@@ -18,7 +18,11 @@ class MainActivity : AppCompatActivity() {
 
             val inputEmail = emailEdt.text.toString()
 
-            Toast.makeText(this, inputEmail, Toast.LENGTH_SHORT).show()
+            // 입력한 아이디는 ***입니다.
+
+            var formattedStr = "입력한 아이디는 ${inputEmail} 입니다"
+
+            Toast.makeText(this, formattedStr, Toast.LENGTH_SHORT).show()
 
         }
 
